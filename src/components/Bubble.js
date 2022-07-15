82c3c08696885dba835945f07fb2f3515c0d251f
cwhoaon <kcwkcw0802@gmail.com>
2022-07-16 00:52:17 +0900

    moongle

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -1,4 +1,34 @@
+import { ThemeConsumer } from "styled-components"
+
 class Bubble {
+    constructor(p, x, y, radius, color) {
+        this.p = p
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.particles = []
+        this.build()
+    }
+
+    build() {
+        for(let i=0; i<500; i++) {
+            let r = Math.random() * this.radius * 0.6
+            let theta = Math.random() * Math.PI * 2
+            this.particles.push(
+                new Particle(this.p, r*Math.cos(theta) + this.x, r*Math.sin(theta) + this.y, this.radius/3, this.color)
+            )
+        }
+    }
+
+    draw () {
+        for(const particle of this.particles) {
+            particle.draw()
+        }
+    }
+}
+
+class Particle {
     constructor(p, x, y, radius, color) {
         this.p = p
         this.x = x
@@ -10,7 +40,7 @@ class Bubble {
     draw () {
         this.p.noStroke()
         this.p.fill(this.color.r, this.color.g, this.color.b)
-        this.p.circle(this.x, this.y, this.radius * 2 * 0.8)
+        this.p.circle(this.x, this.y, this.radius * 2)
     }
 }
 
